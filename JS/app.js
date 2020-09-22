54c938d83ba5bb24182841d87b63b0913b8acf53
Matt Dillemuth <mattdillemuth@gmail.com>
2020-09-21 21:29:20 -0400

    Add pokemonList array containing 9 pokemon objects

---
+++ JS/app.js
@@ -0,0 +1,76 @@
+let pokemonList = [];
+
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 0.7,
+  type: ['grass', 'poison'],
+  hp: 45,
+  atk: 49,
+  def: 49
+}
+pokemonList[1] = {
+  name: "Ivysaur",
+  height: 1,
+  type: ['grass', 'poison'],
+  hp: 60,
+  atk: 62,
+  def: 63
+}
+pokemonList[2] = {
+  name: "Venusaur",
+  height: 2,
+  type: ['grass', 'poison'],
+  hp: 80,
+  atk: 82,
+  def: 83
+}
+pokemonList[3] = {
+  name: "Charmander",
+  height: 0.6,
+  type: ['fire'],
+  hp: 39,
+  atk: 52,
+  def: 43
+}
+pokemonList[4] = {
+  name: "Charmeleon",
+  height: 1.1,
+  type: ['fire'],
+  hp: 58,
+  atk: 64,
+  def: 58
+}
+pokemonList[5] = {
+  name: "Charizard",
+  height: 1.7,
+  type: ['fire', 'flying'],
+  hp: 78,
+  atk: 84,
+  def: 78
+}
+pokemonList[6] = {
+  name: "Squirtle",
+  height: 0.5,
+  type: ['water'],
+  hp: 44,
+  atk: 48,
+  def: 65
+}
+pokemonList[7] = {
+  name: "Wartortle",
+  height: 1,
+  type: ['water'],
+  hp: 59,
+  atk: 63,
+  def: 80
+}
+pokemonList[8] = {
+  name: "Blastoise",
+  height: 1.6,
+  type: ['water'],
+  hp: 79,
+  atk: 83,
+  def: 100
+}
+
+console.log(pokemonList);
