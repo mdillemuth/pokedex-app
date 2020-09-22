06f79c5620d456ec3a42a16f1c6414406d7ac53f
Matt Dillemuth <mattdillemuth@gmail.com>
2020-09-22 16:38:23 -0400

    Add functionality to write pokemonList to DOM

--- JS/app.js
+++ JS/app.js
@@ -4,101 +4,112 @@ const navEl = document.querySelector(".header__nav");
 const hamburgerBarsEl = document.getElementsByTagName("span");
 
 function toggleNav() {
-      navTriggerEl.addEventListener("click", function() {
-              navEl.classList.toggle("shift");
-              animateHamburgers();
-            });
+  //Toggles appearance of the side nav on mobile when hamburger clicked
+  navTriggerEl.addEventListener("click", function () {
+    navEl.classList.toggle("shift");
+    animateHamburgers();
+  });
 }
 
 function animateHamburgers() {
-      for (let item of hamburgerBarsEl) {
-              item.classList.toggle("change");
-            }
+  //Toggles hamburger icon to 'X' when side nav appears
+  for (let item of hamburgerBarsEl) {
+    item.classList.toggle("change");
+  }
 }
 
 toggleNav();
 
 /* *===== POKEDEX SCRIPTS =====* */
-
 let pokemonList = [
   {
     name: "Bulbasaur",
     height: 0.7,
-    type: ['grass', 'poison'],
+    type: ["grass", "poison"],
     hp: 45,
     atk: 49,
-    def: 49
+    def: 49,
   },
   {
     name: "Ivysaur",
     height: 1,
-    type: ['grass', 'poison'],
+    type: ["grass", "poison"],
     hp: 60,
     atk: 62,
-    def: 63
+    def: 63,
   },
   {
     name: "Venusaur",
     height: 2,
-    type: ['grass', 'poison'],
+    type: ["grass", "poison"],
     hp: 80,
     atk: 82,
-    def: 83
+    def: 83,
   },
   {
     name: "Charmander",
     height: 0.6,
-    type: ['fire'],
+    type: ["fire"],
     hp: 39,
     atk: 52,
-    def: 43
+    def: 43,
   },
   {
     name: "Charmeleon",
     height: 1.1,
-    type: ['fire'],
+    type: ["fire"],
     hp: 58,
     atk: 64,
-    def: 58
+    def: 58,
   },
   {
     name: "Charizard",
     height: 1.7,
-    type: ['fire', 'flying'],
+    type: ["fire", "flying"],
     hp: 78,
     atk: 84,
-    def: 78
+    def: 78,
   },
   {
     name: "Squirtle",
     height: 0.5,
-    type: ['water'],
+    type: ["water"],
     hp: 44,
     atk: 48,
-    def: 65
+    def: 65,
   },
   {
     name: "Wartortle",
     height: 1,
-    type: ['water'],
+    type: ["water"],
     hp: 59,
     atk: 63,
-    def: 80
+    def: 80,
   },
   {
     name: "Blastoise",
     height: 1.6,
-    type: ['water'],
+    type: ["water"],
     hp: 79,
     atk: 83,
-    def: 100
-  }
+    def: 100,
+  },
 ];
 
-function writeName () {
+function writeName() {
+  //Iterates through content of pokemonList[] and writes to DOM
   for (i in pokemonList) {
-    document.write(`Pokemon Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}m)<br>`);
+    if (pokemonList[i].height >= 2) {
+      //Checks for Pokemon with height >= 2m 
+      document.write(
+        `Pokemon Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}m) -Wow, that's big!<br></br>`
+      );
+    } else {
+      document.write(
+        `Pokemon Name: ${pokemonList[i].name} (height: ${pokemonList[i].height}m)<br></br>`
+      );
+    }
   }
 }
 
-// writeName();
+writeName();
